ac99f0d7c81150c2a4881fb74764bcf2621bbeae
Prasad Joshi <ppjoshi2003@gmail.com>
2024-09-02 20:35:21 +0530

    added dotenv and JWT_SECRET made real secret

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ const app = express();
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 const cors = require("cors")
-const JWT_SECRET = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+require("dotenv").config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 app.use(express.json());
 app.use(cors());
